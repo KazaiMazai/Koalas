8e2e144e88b914562e376f4108eb3f83dcd2e4f4
Sergey Kazakov <kazaimazai@gmail.com>
2020-06-06 17:54:42 +0300

    added tests list

--- Tests/KoalasTests/KoalasTests.swift
+++ Tests/KoalasTests/KoalasTests.swift
@@ -2,14 +2,6 @@ import XCTest
 @testable import Koalas
 
 final class KoalasTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Koalas().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    static var allTests = DataSeriesTests.allTests
 }
