643a963f4da5303b2157996cef13b068b91d0e67
Sergey Kazakov <kazaimazai@gmail.com>
2020-07-03 20:58:46 +0300

    implemented zip for two series

--- Sources/Koalas/DataSeries/DataSeries.swift
+++ Sources/Koalas/DataSeries/DataSeries.swift
@@ -42,3 +42,16 @@ public func zipSeries<T1, T2, T3>(s1: DataSeries<T1>?, s2: DataSeries<T2>?, s3: 
     let result = zip(s1, zip(s2, s3)).map { Tuple3(t1: $0.0, t2: $0.1.0, t3: $0.1.1) }
     return DataSeries(result)
 }
+
+public func zipSeries<T1, T2>(s1: DataSeries<T1>?, s2: DataSeries<T2>?) -> DataSeries<Tuple2<T1?, T2?>>? {
+    guard let s1 = s1,
+        let s2 = s2
+    else {
+        return nil
+    }
+
+    assert(s1.count == s2.count, "Dataseries should have equal length")
+
+    let result = zip(s1, s2).map { Tuple2(t1: $0.0, t2: $0.1) }
+    return DataSeries(result)
+}
